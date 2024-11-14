2bb058e2a080d0059ce5534edd013a28e4061520
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-11-14 17:14:35 +0200

    assets and App.vue upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <HeaderSection />
+    <HomeSection />
+    <FeaturesSection />
+    <FindOutSection />
+    <FooterSection />
+  </main>
+</template>
+
+<script setup>
+import HeaderSection from './components/HeaderSection.vue'
+import HomeSection from './components/HomeSection.vue'
+import FeaturesSection from './components/FeaturesSection.vue'
+import FindOutSection from './components/FindOutSection.vue'
+import FooterSection from './components/FooterSection.vue'
+</script>
+
+<style scoped></style>
